3adf220b330037fe2ee34e74cfa4f957f600602c
chandrasekharnayak <k.csnayak108@gmail>
2025-01-06 08:15:29 +0530

    v1.1

---
+++ Prac/datatype.py
@@ -0,0 +1,93 @@
+'''14types of datatype
+
+string
+list
+dictionary
+range
+
+operator
+'''
+from reportlab.graphics.barcode.code128 import starta
+
+# st = 'fdfYGY4564^%$%'
+# print(type(st))
+# slicing and methods
+'''python is 0 based index . +ve(0) , -ve(-1) slicing'''
+
+# var[start:end+1:steps]
+st = 'Bhubaneswar'
+
+'''s = st[0:4:1]
+print(s)'''
+'''s= st[::-1]
+print(s)'''
+
+print(dir(st))
+
+
+#split, strip, join
+
+'''st = 'Abhiman Abhilash Abhilipsha Abhiram'
+
+var = st.split()
+print(var)
+'''
+
+'''li = ['Abhiman', 'Abhilash', 'Abhilipsha', 'Abhiram']
+
+st = ' '.join(li)
+print(st)'''
+
+'''st = '   jkbhu      '
+print(st.lstrip())'''
+
+#list --- data structure
+
+'''var = [89,78,23,21,56,43,43,43]
+print(type(var))
+print(dir(var))'''
+
+'''li = var.copy()
+print(li)'''
+
+'''tu = (10,20,3,2,1,2,1,1,1,1,1)
+print(type(tu))
+print(dir(tu))'''
+
+#dictionary
+
+# di = {'Name':'Mahesh','Age':23,'Salary':67000,'ManagerName':'Mahesh'}
+# 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values']
+
+'''var = di.update({'A':1,'B':2,'C':3})
+print(di)'''
+
+'''di['DEPT']='QA'
+print(di)'''
+
+
+#Operator
+
+# 1.Arithmetic (+,-,*,/,//,**,%)
+# print(10%3)
+
+#Relational or coparison (>,>=,<,<=, == ,!=)
+
+#logical op (and , or , not)
+
+#Assignment
+
+#special op :- idenitity op , membership
+
+# a = 10
+# b = 11
+# print(a is b)
+
+li = [10,20,30,40]
+print(100 in li)
+
+
+condition and for
+ 7 and 8 ---- for and if  9-puja
+
+ 10-- function ---
